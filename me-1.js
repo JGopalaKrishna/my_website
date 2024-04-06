d54a75d6c8605fd9dee409d8c99679061db0eb42
JGopalaKrishna <141515852+JGopalaKrishna@users.noreply.github.com>
2024-04-06 20:07:56 +0530

    uploaded files

---
+++ me-1.js
@@ -0,0 +1,23 @@
+const ent1 = document.getElementById("n1")
+const ent2 = document.getElementById("n2")
+const par = document.getElementById("p1")
+
+function add(a,b){
+    let c = a+b;
+    par.innerText=`Addtion of ${a} and ${b} = ${c}`
+}
+
+function sub(a,b){
+    let c = a-b;
+    par.innerText=`Substraction of ${a} and ${b} = ${c}`
+}
+
+function mul(a,b){
+    let c = a*b;
+    par.innerText=`Multipilication of ${a} and ${b} = ${c}`
+}
+
+function div(a,b){
+    let c = a/b;
+    par.innerText=`Division of ${a} and ${b} = ${c}`
+}
